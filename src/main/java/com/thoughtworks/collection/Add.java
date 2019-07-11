bca9e3faabd0cbf48c96a6637a5cd0afec24b43a
曹桂凤 <demi.cao@oocl.com>
2019-07-11 17:41:09 +0800

    feat:not finished

--- src/main/java/com/thoughtworks/collection/Add.java
+++ src/main/java/com/thoughtworks/collection/Add.java
@@ -2,19 +2,33 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
 
 public class Add {
     public int getSumOfEvens(int leftBorder, int rightBorder) {
-        throw new NotImplementedException();
+
+        int min = Math.min(leftBorder,rightBorder);
+        int max = Math.max(leftBorder,rightBorder);
+        return IntStream.rangeClosed(min,max).filter(num->num%2==0).sum();
+
     }
 
     public int getSumOfOdds(int leftBorder, int rightBorder) {
-        throw new NotImplementedException();
+        int min = Math.min(leftBorder,rightBorder);
+        int max = Math.max(leftBorder,rightBorder);
+        return IntStream.rangeClosed(min,max).filter(num->num%2!=0).sum();
     }
 
     public int getSumTripleAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+//        List<String> arrayBefore = Arrays.asList("a", "b", "c", "d");
+//        List<String> arrayAfter = arrayBefore.stream() .map(String::toUpperCase) .collect(toList());
+        return arrayList.stream().map(s -> 3*s);
     }
 
     public List<Integer> getTripleOfOddAndAddTwo(List<Integer> arrayList) {
